aca59f5559b1fbcb2f1f1f2c0c4d30878b64e260
Riya Vishwakarma <riyavishwakarma.alf@gmail.com>
2024-02-01 21:07:55 +0530

    components => about us

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import About from "@/components/About";
 import Category from "@/components/Category";
 import Choose from "@/components/Choose";
 import Faq from "@/components/Faq";
@@ -8,6 +9,7 @@ export default function Home() {
   return (
     <div className="max-w-7xl mx-auto p-4">
       <Herobanner />
+      <About />
       <Category />
       <Choose />
       <Testimonials />
---
+++ src/components/About.tsx
@@ -0,0 +1,66 @@
+import Image from "next/image";
+import React from "react";
+
+const content = [
+  {
+    src: "/assets/about/festival.webp",
+    title: "Traditional gifting",
+    content: "Festive Aromas, Celebrate Traditions. Gift the Essence of Joy.",
+  },
+  {
+    src: "/assets/about/wedding.webp",
+    title: "Wedding gifting",
+    content: "Scented Moments of Love. Perfume the Beginnings of Forever.",
+  },
+  {
+    src: "/assets/about/corporate-gifting.webp",
+    title: "Corporate gifting",
+    content:
+      "Elevate Business Bonds with Signature Scents. Unforgettable Impressions, Every Gift.",
+  },
+  {
+    src: "/assets/about/customize.webp",
+    title: "Customize gifting",
+    content:
+      "Craft Memories with Fragrance. Customized for Every Special Occasion.",
+  },
+];
+
+const About = () => {
+  return (
+    <section className="space-y-6 py-5">
+      <div className="border-b-2 pb-2 border-primary">
+        <p className="text-4xl font-bold">About Us</p>
+      </div>
+      <p>
+        Integrate embodies the spirit of personalized, traditional, and
+        corporate gifting, elevating the experience of giving and receiving
+        fragrances.
+      </p>
+      <div className="grid md:grid-cols-2 place-items-center gap-5">
+        {content.map((item, i) => (
+          <div key={i} className="grid md:grid-cols-2 gap-5 place-items-center">
+            <div className="relative h-[250px] w-[250px]">
+              <Image
+                src={item.src}
+                className="object-cover rounded-xl"
+                fill
+                priority
+                alt={item.title}
+              />
+            </div>
+            <div className="flex flex-col justify-center h-[250px] w-[250px] rounded-xl bg-gray-100 items-center gap-4 p-5 ">
+              <p className="text-center text-sm">
+                <span className="font-semibold text-base">{item.title}</span>{" "}
+                <br />
+                &quot;{item.content}&quot;
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default About;
