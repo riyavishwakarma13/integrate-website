33589bbba84305bd0a6fdf0b38e467d8db74b803
Riya Vishwakarma <riyavishwakarma.alf@gmail.com>
2024-02-03 00:34:53 +0530

    changes => navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,13 +3,13 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="sticky top-0 z-50 bg-primary shadow-less">
+    <nav className="sticky top-0 z-50 py-2 bg-primary shadow-less">
       <div className="flex items-center justify-center">
         <Image
           src={"/assets/logo.webp"}
-          height={100}
+          height={150}
           priority
-          width={200}
+          width={300}
           alt="logo"
         />
       </div>
